f2380b8f0572f5b94cc34a58fc0dee62dc5065fa
orfrenkel <orf@zofim.org.il>
2014-12-03 22:47:31 +0200

    fix exception

---
+++ src/Model/MangerGame.java
@@ -0,0 +1,30 @@
+package Model;
+
+public class MangerGame {
+	 // static varibel save the number of games play in courrect round
+	private static Integer numOfGame=0;
+
+
+
+
+
+///////////////////////////////////////////////////methods/////////////////////////////
+
+/**
+ * do ++ to number of games
+ * todo: finished method iteration 2
+ */
+public void newGame()
+{
+	numOfGame++;
+}
+/**
+ * intalize zero to number of games
+ * todo: finished method iteration 2
+ */
+public void newRound()
+{
+	numOfGame=0;
+}
+
+}
